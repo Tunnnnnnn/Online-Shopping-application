109ba5714d38fa4ad8a07f78f1dd5c9562dff17d
Tun Kanokwiroon <141589679+Tunnnnnnn@users.noreply.github.com>
2023-08-13 23:07:26 +1200

    Add files via upload
    UL1: PC parts and Laptop obj added

---
+++ Assignment1/ComputerStore.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Assignment1;
+
+/**
+ *
+ * @author jamez
+ */
+
+import java.util.*;
+
+public class ComputerStore 
+{    
+    public static void main(String[] args) 
+    {    
+        Menu.menu();
+   
+    }
+}
